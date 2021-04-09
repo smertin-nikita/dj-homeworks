1610640ba609aace672d8b59214dc52859fc42ba
Smertin Nikita Job <smertin-nikita@mail.ru>
2021-04-09 14:51:37 +0500

    Added filename converter

--- request-handling/file_server/app/converters.py
+++ request-handling/file_server/app/converters.py
@@ -1,16 +1,29 @@
+import os
 from datetime import datetime
+from app import settings
 
 
 class DatetimeConverter:
     regex = r'[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}'
     format = '%Y-%m-%d'
 
-    def to_python(self, value):
+    def to_python(self, value: str) -> datetime:
         return datetime.strptime(value, self.format)
 
-    def to_url(self, value):
+    def to_url(self, value: datetime) -> str:
         return value.strftime(self.format)
 
 
 class FilenameConverter:
-    pass
+    regex = r'[\w.]+'
+    # если знаем что все файлы называются server.[0-9]{2} можно сделать регулярку
+    # regex = r'server\.[0-9]{2}'
+
+    def to_python(self, value: str) -> str:
+        if os.path.exists(os.path.join(settings.FILES_PATH, 'files')):
+            return value
+        else:
+            raise ValueError("File doesn't exist")
+
+    def to_url(self, value: str) -> str:
+        return value
