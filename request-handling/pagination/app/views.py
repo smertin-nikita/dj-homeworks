062bbb4c3197a4893485292b6a780336838ca9bc
Smertin Nikita Job <smertin-nikita@mail.ru>
2021-04-09 10:52:06 +0500

    Gets prev page url

--- request-handling/pagination/app/views.py
+++ request-handling/pagination/app/views.py
@@ -9,11 +9,12 @@ def index(request):
 def bus_stations(request, page=1):
     current_page = page
     next_page_url = reverse('bus_stations', kwargs={'page': current_page + 1})
+    prev_page_url = reverse('bus_stations', kwargs={'page': current_page - 1})
     return render(request, 'index.html', context={
         'bus_stations': [{'Name': 'название', 'Street': 'улица', 'District': 'район'},
                          {'Name': 'другое название', 'Street': 'другая улица', 'District': 'другой район'}],
         'current_page': current_page,
-        'prev_page_url': None,
+        'prev_page_url': prev_page_url if current_page > 1 else None,
         'next_page_url': next_page_url,
     })
 
