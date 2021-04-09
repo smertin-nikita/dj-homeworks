d714c30c3e7425e43972e661bd22749fa961f6ea
Smertin Nikita Job <smertin-nikita@mail.ru>
2021-04-09 12:52:29 +0500

    fixed: get next and previous page

--- request-handling/pagination/app/views.py
+++ request-handling/pagination/app/views.py
@@ -14,19 +14,27 @@ def index(request):
 def bus_stations(request, page_number=1):
     chunk = 10
 
-    next_page_url = reverse('bus_stations', kwargs={'page_number': page_number + 1})
-    prev_page_url = reverse('bus_stations', kwargs={'page_number': page_number - 1})
-
     # Так как файл относительно не большой - читаем целиком
     with open(settings.BUS_STATION_CSV, encoding='cp1251') as csvfile:
         reader = csv.DictReader(csvfile)
         paginator = Paginator(list(iter(reader)), chunk)
 
     page = paginator.get_page(page_number)
+
+    if page.has_next():
+        next_page_url = reverse('bus_stations', kwargs={'page_number': page.next_page_number()})
+    else:
+        next_page_url = None
+
+    if page.has_previous():
+        prev_page_url = reverse('bus_stations', kwargs={'page_number': page.previous_page_number()})
+    else:
+        prev_page_url = None
+
     return render(request, 'index.html', context={
         'bus_stations': page.object_list,
         'current_page': page_number,
-        'prev_page_url': prev_page_url if page_number > 1 else None,
+        'prev_page_url': prev_page_url,
         'next_page_url': next_page_url,
     })
 
