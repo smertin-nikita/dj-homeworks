1e3c644bf718b3df0d62a6f20f2195a3647ca49d
Smertin Nikita <smertin-nikita@mail.ru>
2021-04-12 22:08:57 +0500

    read csv file

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -1,10 +1,16 @@
+import csv
+import os
+
 from django.shortcuts import render
 
+from app import settings
+
+
 def inflation_view(request):
-    template_name = 'inflation.html'
+    template_name = 'app/inflation.html'
 
     # чтение csv-файла и заполнение контекста
-    context = {}
+    with open(os.path.join(settings.BASE_DIR, 'inflation_russia.csc')) as csvfile:
+        context = csv.DictReader(csvfile)
 
-    return render(request, template_name,
-                  context)
+    return render(request, template_name, context)
