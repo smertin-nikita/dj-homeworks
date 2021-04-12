a5e14adadef3ececd01a1a533747f8dc2a179b18
Smertin Nikita <smertin-nikita@mail.ru>
2021-04-12 23:10:50 +0500

    don't work tag for

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -10,7 +10,9 @@ def inflation_view(request):
     template_name = 'app/inflation.html'
 
     # чтение csv-файла и заполнение контекста
-    with open(os.path.join(settings.BASE_DIR, 'inflation_russia.csc')) as csvfile:
-        context = csv.DictReader(csvfile)
+    with open(os.path.join(settings.BASE_DIR, 'inflation_russia.csv'), encoding='utf-8') as csvfile:
+        reader = iter(csv.reader(csvfile))
+        context = [item for item in reader]
+        print(context[0])
 
-    return render(request, template_name, context)
+    return render(request, template_name, {'keys': context[0], 'content': context})
