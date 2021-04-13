2fac8081de63abdc906310510506764537a1fa75
Smertin Nikita <smertin-nikita@mail.ru>
2021-04-13 23:24:05 +0500

    Shows active menu

--- dynamic-templates/task2/app/templatetags/app_tags.py
+++ dynamic-templates/task2/app/templatetags/app_tags.py
@@ -3,3 +3,14 @@ from django import template
 
 register = template.Library()
 
+
+@register.inclusion_tag('app/menu.html')
+def show_menu(active_page=None):
+    if active_page:
+        context = dict()
+        context[active_page] = 'class=active'
+        return context
+
+
+
+
