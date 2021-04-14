f78e04dc7d8db7832c3ac5f17293cd5492a3689e
Smertin Nikita Job <smertin-nikita@mail.ru>
2021-04-14 14:12:11 +0500

    Added date filter

--- dynamic-templates/task3/app/templatetags/news_filters.py
+++ dynamic-templates/task3/app/templatetags/news_filters.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from django import template
 
 
@@ -6,8 +8,24 @@ register = template.Library()
 
 @register.filter
 def format_date(value):
-    # Ваш код
-    return value
+    date = datetime.utcfromtimestamp(value)
+
+    duration = datetime.utcnow() - date
+    minutes = duration.total_seconds() / 60
+    hours = duration.total_seconds() / 3600
+    print(duration, minutes, hours)
+    if minutes < 10:
+        return "только что"
+    elif minutes < 60:
+        return f"{minutes:.0f} минут назад"
+    elif hours < 2:
+        return f"{hours:.0f} час назад"
+    elif hours < 5:
+        return f"{hours:.0f} часа назад"
+    elif hours < 24:
+        return f"{hours:.0f} часов назад"
+    elif hours > 24:
+        return date.strftime('%Y-%d-%m')
 
 
 # необходимо добавить фильтр для поля `score`
