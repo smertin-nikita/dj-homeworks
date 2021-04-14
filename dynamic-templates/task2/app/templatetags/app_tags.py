3811827b9ed485ddd7ead7dc55fab5acdb7aa5ce
Smertin Nikita Job <smertin-nikita@mail.ru>
2021-04-14 10:21:40 +0500

    Added comment

--- dynamic-templates/task2/app/templatetags/app_tags.py
+++ dynamic-templates/task2/app/templatetags/app_tags.py
@@ -6,6 +6,10 @@ register = template.Library()
 
 @register.inclusion_tag('app/menu.html')
 def show_menu(active_page=None):
+    """
+    :param active_page: Активная страница. Название шаблона который отображается
+    :return:
+    """
     if active_page:
         context = dict()
         context[active_page] = 'class=active'
