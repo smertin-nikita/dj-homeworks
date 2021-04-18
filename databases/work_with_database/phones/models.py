1b19302f1d96fcf9c6d1c67c1c278749cff263ca
Smertin Nikita <smertin-nikita@mail.ru>
2021-04-18 20:05:05 +0500

    created models

--- databases/work_with_database/phones/models.py
+++ databases/work_with_database/phones/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.TextField()
+    image = models.URLField()
+    price = models.IntegerField()
+    date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(unique=True)
