98c5886718212551846ae384434e184fae5a254b
Smertin Nikita <smertin-nikita@mail.ru>
2021-04-18 19:33:53 +0500

    added database logging

--- databases/work_with_database/main/settings.py
+++ databases/work_with_database/main/settings.py
@@ -129,3 +129,21 @@ try:
     from .settings_local import *
 except ImportError:
     pass
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
