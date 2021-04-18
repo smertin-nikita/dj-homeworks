3b476eefc8fab4427146b447668919241a9b7d8f
Smertin Nikita <smertin-nikita@mail.ru>
2021-04-18 21:15:48 +0500

    Fixed field name

--- databases/work_with_database/phones/models.py
+++ databases/work_with_database/phones/models.py
@@ -5,6 +5,6 @@ class Phone(models.Model):
     name = models.TextField()
     image = models.URLField()
     price = models.IntegerField()
-    date = models.DateField()
+    release_date = models.DateField()
     lte_exists = models.BooleanField()
     slug = models.SlugField(unique=True)
