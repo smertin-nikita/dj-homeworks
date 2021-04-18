d71c1544aff0c675a290bab70b3fe685d8973fcc
Smertin Nikita <smertin-nikita@mail.ru>
2021-04-18 22:09:09 +0500

    added context

--- databases/work_with_database/phones/views.py
+++ databases/work_with_database/phones/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
+from phones.models import Phone
+
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    context = {'phones': list(Phone.objects.all())}
     return render(request, template, context)
 
 
