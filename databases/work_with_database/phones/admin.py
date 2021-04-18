9ce786235728efbd2e87d7b39a3604624c760a0a
Smertin Nikita <smertin-nikita@mail.ru>
2021-04-18 20:57:42 +0500

    Registered model phone to admin

--- databases/work_with_database/phones/admin.py
+++ databases/work_with_database/phones/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from phones.models import Phone
+
+
+class PhoneAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+
+admin.site.register(Phone, PhoneAdmin)
