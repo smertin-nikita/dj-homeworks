83841dcde93922a9ed567deaa7fccf9d297d0e46
Smertin Nikita <smertin-nikita@mail.ru>
2021-04-20 23:29:24 +0500

    Start valid logic

--- databases_2/m2m-relations/articles/admin.py
+++ databases_2/m2m-relations/articles/admin.py
@@ -1,8 +1,31 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, ArticleTag, Tag
+
+admin.site.register(Tag)
+
+
+class ArticleTagInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        for form in self.forms:
+            # В form.cleaned_data будет словарь с данными
+            # каждой отдельной формы, которые вы можете проверить
+            print(form.cleaned_data)
+            # вызовом исключения ValidationError можно указать админке о наличие ошибки
+            # таким образом объект не будет сохранен,
+            # а пользователю выведется соответствующее сообщение об ошибке
+            raise ValidationError('Укажите основной раздел')
+            raise ValidationError('Основным может быть только один раздел')
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+
+class ArticleTagInline(admin.TabularInline):
+    model = ArticleTag
+    formset = ArticleTagInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ArticleTagInline]
