837828298dbee6b408358ee33b13002e399dd973
Smertin Nikita <smertin-nikita@mail.ru>
2021-04-21 21:05:35 +0500

    Badge primery works

--- databases_2/m2m-relations/articles/views.py
+++ databases_2/m2m-relations/articles/views.py
@@ -11,7 +11,7 @@ def articles_list(request):
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = '-published_at'
 
-    articles = Article.objects.all().order_by(ordering).prefetch_related('tags')
+    articles = Article.objects.prefetch_related('articletag_set').order_by(ordering)
     context = {'articles': articles}
 
     return render(request, template, context)
