67a67570063ecd9a88e1210505f25dc4359ada77
Smertin Nikita Job <smertin-nikita@mail.ru>
2021-04-21 15:51:07 +0500

    Tag badge don't work

--- databases_2/m2m-relations/articles/views.py
+++ databases_2/m2m-relations/articles/views.py
@@ -6,10 +6,12 @@ from articles.models import Article
 
 def articles_list(request):
     template = 'articles/news.html'
-    context = {}
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = '-published_at'
+
+    articles = Article.objects.all().order_by(ordering).prefetch_related('tags')
+    context = {'articles': articles}
 
     return render(request, template, context)
