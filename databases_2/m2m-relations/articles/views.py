08ddf01e24c16241608f3cdc1f9cea393bb1f28d
Smertin Nikita <smertin-nikita@mail.ru>
2021-04-21 21:23:47 +0500

    Added todo: how to optimize queries?

--- databases_2/m2m-relations/articles/views.py
+++ databases_2/m2m-relations/articles/views.py
@@ -10,7 +10,7 @@ def articles_list(request):
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = '-published_at'
-
+    # todo Можно ли оптимизировать запросы к бд?
     articles = Article.objects.prefetch_related('articletag_set').order_by(ordering)
     context = {'articles': articles}
 
