e184564b364cdaffa13d2858969793d797bcb78b
Smertin Nikita Job <smertin-nikita@mail.ru>
2021-04-22 10:20:59 +0500

    Fix LANGUAGE_CODE

--- databases_2/orm_migrations/website/settings.py
+++ databases_2/orm_migrations/website/settings.py
@@ -97,7 +97,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'RU-ru'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'Europe/Moscow'
 
