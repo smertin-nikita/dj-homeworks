ada4290434c9fdeba08822aa733f87e5027ec1d8
Smertin Nikita Job <smertin-nikita@mail.ru>
2021-04-22 11:11:04 +0500

    Added inline

--- databases_2/orm_migrations/school/admin.py
+++ databases_2/orm_migrations/school/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 from .models import Student, Teacher
 
 
+class TeacherStudentInline(admin.TabularInline):
+    model = Student.teachers.through
+
+
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     pass
@@ -10,4 +14,4 @@ class StudentAdmin(admin.ModelAdmin):
 
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
-    pass
+    inlines = [TeacherStudentInline]
