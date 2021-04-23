85bf65b157123e33639e1c6a03abbde4ed7991ac
Smertin Nikita Job <smertin-nikita@mail.ru>
2021-04-23 10:57:19 +0500

    Optimized queries

--- databases_2/m2m-relations/articles/views.py
+++ databases_2/m2m-relations/articles/views.py
@@ -1,17 +1,19 @@
+from django.db.models import Prefetch
 from django.views.generic import ListView
 from django.shortcuts import render
 
-from articles.models import Article
+from articles.models import Article, ArticleTag
 
 
 def articles_list(request):
     template = 'articles/news.html'
 
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = '-published_at'
-    # todo Можно ли оптимизировать запросы к бд?
-    articles = Article.objects.prefetch_related('articletag_set').order_by(ordering)
+
+    # queryset для тегов с инфой из промежуточной модели ArticleTag и основной Tag c сортировкой по главному тегу
+    articletag_set = ArticleTag.objects.order_by('-major').select_related('tag')
+
+    articles = Article.objects.order_by(ordering).prefetch_related(Prefetch('articletag_set', queryset=articletag_set))
     context = {'articles': articles}
 
     return render(request, template, context)
