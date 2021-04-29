e1e268fbd11fea47895c4c8f94c6d270dce3ab10
Smertin Nikita <smertin-nikita@mail.ru>
2021-04-29 22:24:36 +0500

    Added project serializer

--- drf-intro/simple_crud/measurements/serializers.py
+++ drf-intro/simple_crud/measurements/serializers.py
@@ -1,1 +1,8 @@
-# TODO: опишите сериализаторы
+from rest_framework.serializers import ModelSerializer
+from measurements.models import Project
+
+
+class ProjectSerializer(ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ['id', 'name', 'latitude', 'longitude', 'created_at', 'updated_at']
