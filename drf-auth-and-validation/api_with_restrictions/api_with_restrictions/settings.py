62f14b3414dfb7e351d7a03ce9cca7cfd06e1e6a
Smertin Nikita <smertin-nikita@mail.ru>
2021-05-09 18:29:42 +0500

    fix: added global filter backend

--- drf-auth-and-validation/api_with_restrictions/api_with_restrictions/settings.py
+++ drf-auth-and-validation/api_with_restrictions/api_with_restrictions/settings.py
@@ -80,6 +80,9 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
     ]
 }
 
@@ -130,4 +133,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-DEFAULT_FILTER_BACKENDS = [DjangoFilterBackend]
+
