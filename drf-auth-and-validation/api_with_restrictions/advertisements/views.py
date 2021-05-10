f08ad657757f4c75b3f6fa07ef62e3532ac532d9
Smertin Nikita <smertin-nikita@mail.ru>
2021-05-10 19:59:05 +0500

    Added permissions IsOwnerOrReadOnly

--- drf-auth-and-validation/api_with_restrictions/advertisements/views.py
+++ drf-auth-and-validation/api_with_restrictions/advertisements/views.py
@@ -1,9 +1,26 @@
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 
 from advertisements.filters import AdvertisementFilter
 from advertisements.models import Advertisement
 from advertisements.serializers import AdvertisementSerializer
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Object-level permission to only allow owners of an object to edit it.
+    Assumes the model instance has an `owner` attribute.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Instance must have an attribute named `creator`.
+        return obj.creator == request.user
 
 
 class AdvertisementViewSet(ModelViewSet):
@@ -15,8 +32,9 @@ class AdvertisementViewSet(ModelViewSet):
     serializer_class = AdvertisementSerializer
     filterset_class = AdvertisementFilter
 
+
     def get_permissions(self):
         """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
-            return [IsAuthenticated()]
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            return [IsAuthenticated(), IsOwnerOrReadOnly()]
         return []
