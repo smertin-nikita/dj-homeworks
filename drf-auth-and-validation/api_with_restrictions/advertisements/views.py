eb3e3133ab4485e9771e9b29974dc939bae4380a
Smertin Nikita <smertin-nikita@mail.ru>
2021-05-11 18:17:25 +0500

    Added permissions for staff

--- drf-auth-and-validation/api_with_restrictions/advertisements/views.py
+++ drf-auth-and-validation/api_with_restrictions/advertisements/views.py
@@ -1,5 +1,5 @@
 from rest_framework import permissions
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, AND, OR
 from rest_framework.viewsets import ModelViewSet
 
 from advertisements.filters import AdvertisementFilter
@@ -7,10 +7,25 @@ from advertisements.models import Advertisement
 from advertisements.serializers import AdvertisementSerializer
 
 
+class IsStaffOrReadOnly(permissions.BasePermission):
+    """
+    Object-level permission to only allow staffs(admins) to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Instance must have an attribute named `creator`.
+        return request.user.is_staff
+
+
 class IsOwnerOrReadOnly(permissions.BasePermission):
     """
     Object-level permission to only allow owners of an object to edit it.
-    Assumes the model instance has an `owner` attribute.
+    Assumes the model instance has an `creator` attribute.
     """
 
     def has_object_permission(self, request, view, obj):
@@ -32,9 +47,8 @@ class AdvertisementViewSet(ModelViewSet):
     serializer_class = AdvertisementSerializer
     filterset_class = AdvertisementFilter
 
-
     def get_permissions(self):
         """Получение прав для действий."""
         if self.action in ["create", "update", "partial_update", "destroy"]:
-            return [IsAuthenticated(), IsOwnerOrReadOnly()]
+            return [IsAuthenticated(),  OR(IsStaffOrReadOnly(), IsOwnerOrReadOnly())]
         return []
