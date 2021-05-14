ea5a1c7f6abdbcf7bdf618c43c85ba1000b76fae
Smertin Nikita Job <smertin-nikita@mail.ru>
2021-05-14 11:53:56 +0500

    Added test: test list courses

--- django-testing/django_testing/tests/students/test_courses_api.py
+++ django-testing/django_testing/tests/students/test_courses_api.py
@@ -1,18 +1,30 @@
 import pytest
 from django.urls import reverse
-from rest_framework.status import HTTP_201_CREATED
+from rest_framework.status import HTTP_201_CREATED, HTTP_200_OK
 from rest_framework.test import APIClient
 
 from students.models import Course
 
 
 @pytest.mark.django_db
-def test_create_course():
+def test_list_courses():
+
+    # arrange
+    course = Course.objects.create(name='test')
+
     url = reverse("courses-list")
     client = APIClient()
+
+    # act
     resp = client.get(url)
-    # assert resp.status_code == HTTP_201_CREATED
+
+    # assert
+
+    assert resp.status_code == HTTP_200_OK
     resp_json = resp.json()
-    # c = Course.objects.create(name='test')
-    # assert c.id
-    # assert c.name == 'test'
+    assert resp_json
+    assert len(resp_json) == 1
+    test_course = resp_json[0]
+    assert test_course['id'] == course.id
+    assert 'name' in test_course
+    assert course.name == 'test'
