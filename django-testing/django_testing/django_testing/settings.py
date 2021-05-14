e35dcbd0dec70c01e72674ece226fe186409e670
Smertin Nikita Job <smertin-nikita@mail.ru>
2021-05-14 11:30:46 +0500

    Changes setting local derictory

--- django-testing/django_testing/django_testing/settings.py
+++ django-testing/django_testing/django_testing/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-from django_testing import settings_local
+from students import settings_local
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
