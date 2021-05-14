8e515936ff0a8ea5d1a6427f664ac1bc614497de
Smertin Nikita <smertin-nikita@mail.ru>
2021-05-14 07:47:19 +0500

    Created test

--- django-testing/django_testing/tests/students/test_courses_api.py
+++ django-testing/django_testing/tests/students/test_courses_api.py
@@ -1,2 +1,18 @@
-def test_example():
-    assert False, "Just test example"
+import pytest
+from django.urls import reverse
+from rest_framework.status import HTTP_201_CREATED
+from rest_framework.test import APIClient
+
+from students.models import Course
+
+
+@pytest.mark.django_db
+def test_create_course():
+    url = reverse("courses-list")
+    client = APIClient()
+    resp = client.get(url)
+    # assert resp.status_code == HTTP_201_CREATED
+    resp_json = resp.json()
+    # c = Course.objects.create(name='test')
+    # assert c.id
+    # assert c.name == 'test'
