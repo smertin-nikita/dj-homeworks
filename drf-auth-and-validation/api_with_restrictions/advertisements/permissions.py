1a97eda025b98f72b2d382e1458ef7621db3e1aa
Smertin Nikita <smertin-nikita@mail.ru>
2021-05-15 20:00:38 +0500

    Remove pemissions

---
+++ drf-auth-and-validation/api_with_restrictions/advertisements/permissions.py
@@ -0,0 +1,32 @@
+from rest_framework import permissions
+
+
+class IsStaffOrReadOnly(permissions.BasePermission):
+    """
+    Object-level permission to only allow staffs(admins) to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Instance must have an attribute named `creator`.
+        return request.user.is_staff
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Object-level permission to only allow owners of an object to edit it.
+    Assumes the model instance has an `creator` attribute.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Instance must have an attribute named `creator`.
+        return obj.creator == request.user
--- drf-auth-and-validation/api_with_restrictions/advertisements/views.py
+++ drf-auth-and-validation/api_with_restrictions/advertisements/views.py
@@ -1,42 +1,11 @@
 from django.db.models import Q
-from rest_framework import permissions
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, AND, OR
+from rest_framework.permissions import IsAuthenticated, OR
 from rest_framework.viewsets import ModelViewSet
 
 from advertisements.filters import AdvertisementFilter
 from advertisements.models import Advertisement
+from advertisements.permissions import IsStaffOrReadOnly, IsOwnerOrReadOnly
 from advertisements.serializers import AdvertisementSerializer
-
-
-class IsStaffOrReadOnly(permissions.BasePermission):
-    """
-    Object-level permission to only allow staffs(admins) to edit it.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        # Instance must have an attribute named `creator`.
-        return request.user.is_staff
-
-
-class IsOwnerOrReadOnly(permissions.BasePermission):
-    """
-    Object-level permission to only allow owners of an object to edit it.
-    Assumes the model instance has an `creator` attribute.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        # Instance must have an attribute named `creator`.
-        return obj.creator == request.user
 
 
 class AdvertisementViewSet(ModelViewSet):
@@ -52,7 +21,6 @@ class AdvertisementViewSet(ModelViewSet):
         """queryset с черновиками только для создателя"""
         user = self.request.user
         return Advertisement.objects.filter(Q(creator=user) & Q(draft=True)) | Advertisement.objects.filter(draft=False)
-
 
 
     def get_permissions(self):
